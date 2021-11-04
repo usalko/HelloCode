f92dd5904f492ae26fbf41d0ea63f59df110fcc5
Ivan Usalko <ivict@rambler.ru>
2021-11-04 20:51:45 +0300

    # Задача dev1

--- dev1/Program.cs
+++ dev1/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int inputArraySize = 9;
+int[] inputArray = new int[inputArraySize];
 
+Console.WriteLine("Input array");
+int index = 0;
+while (index < inputArraySize)
+{
+    inputArray[index] = new Random().Next(10, 100); // [10, 99]
+    Console.Write(inputArray[index] + " "); 
+    index++;
+}
+Console.WriteLine();
+
+int sum = 0; 
+index = 0;
+while (index < inputArraySize)
+{
+    sum = sum + inputArray[index];
+    index++;
+}
+double average = sum / (double) inputArraySize;
+Console.WriteLine($"Average sum is {average}");
+
+int[] outputArray = new int[inputArraySize];
+index = 0;
+int outputArraySize = 0;
+while (index < inputArraySize)
+{
+    if (index == 0)
+    {
+        outputArray[outputArraySize] = inputArray[index];
+        outputArraySize++;
+    }
+    else if (inputArray[index] >= outputArray[outputArraySize - 1] &&
+            inputArray[index] <= average &&
+            inputArray[index] % 2 > 0)
+    {
+        outputArray[outputArraySize] = inputArray[index];
+        outputArraySize++;
+    }
+    index++;
+}
+
+Console.WriteLine("Output array");
+index = 0;
+while (index < outputArraySize)
+{
+    Console.Write(outputArray[index] + " ");
+    index++;
+}
+Console.WriteLine();
